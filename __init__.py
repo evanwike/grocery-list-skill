2288b061eaa31e8edaded83302a902d220e9feed
Evan W <sportsdude716@gmail.com>
2019-04-07 00:47:54 -0500

    Updates...

--- __init__.py
+++ __init__.py
@@ -8,11 +8,23 @@ class GroceryList(MycroftSkill):
         MycroftSkill.__init__(self)
         self.grocery_list = []
 
+    # Add item to grocery list
     @intent_file_handler('add_item.intent')
     def handle_add_item_intent(self, message):
         item = message.data.get("item")
         self.grocery_list.append(item)
         self.speak_dialog("add_success", data={'item': item})
+
+    # Remove item from grocery list
+    @intent_file_handler('remove_item.intent')
+    def handle_remove_item_intent(self, message):
+        item = message.data.get('item')
+
+        if item not in self.grocery_list:
+            self.speak_dialog('remove_error')
+        else:
+            self.grocery_list.remove(item)
+            self.speak_dialog('remove_success', data={'item': item})
 
     @intent_file_handler('list.grocery.intent')
     def handle_list_grocery(self, message):
