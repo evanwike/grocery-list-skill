908b0ae27ec39ce0114f89a28dc97a45c9102367
Evan W <sportsdude716@gmail.com>
2019-04-06 22:40:06 -0500

    Test

--- __init__.py
+++ __init__.py
@@ -5,6 +5,17 @@ class GroceryList(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
+        grocery_list = []
+
+    @intent_file_handler('add_item.intent')
+    def handle_add_item_intent(self, message):
+        try:
+            item = message.data.get("item")
+            self.grocery_list.append(item)
+            self.speak_dialog("add_success", data=item)
+        except:
+            self.speak_dialog("add_error")
+
     @intent_file_handler('list.grocery.intent')
     def handle_list_grocery(self, message):
         self.speak_dialog('list.grocery')
@@ -12,4 +23,3 @@ class GroceryList(MycroftSkill):
 
 def create_skill():
     return GroceryList()
-
